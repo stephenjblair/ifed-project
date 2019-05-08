7f0a4e0a4aa6a9d7aabcaaa140ce3b9c1118f559
Blair <stephenjohnblair@outlook.com>
2019-05-08 14:12:09 +0000

    Added addResult function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -253,5 +253,17 @@ var map, places, infoWindow;
           markers[i].setMap(map);
         };
       }
+  
+      function addResult(result, i) {
+        var results = document.getElementById('results');
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+
+        var tr = document.createElement('tr');
+        tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
+        tr.onclick = function() {
+          google.maps.event.trigger(markers[i], 'click');
+        };
+      }    
               
               
